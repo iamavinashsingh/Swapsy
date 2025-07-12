04b8787ce33d5ed713581a31652ca34f1a3b4abc
Avinash Singh <imavinashsingh2@gmail.com>
2025-07-12 11:43:59 +0530

    Frontend Part Initiated

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-gray-900 min-h-screen'>
+      <h1 className='text-red-500 text-4xl'>Hello Swapsy</h1>
+    </div>
+  )
+}
+
+export default App
