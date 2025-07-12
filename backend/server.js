48a6d98114045ebe0919fd2abb8ee8edfeb9770a
Avinash Singh <imavinashsingh2@gmail.com>
2025-07-12 10:25:58 +0530

    Initial Commit

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+// server.js
+const app = require('./app');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+dotenv.config();
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
